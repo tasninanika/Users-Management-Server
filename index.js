0a16eeb2f6d3434258f218705f6274343bcc1ba5
anikatasnin <jarintasnin09@gmail.com>
2025-03-25 20:15:17 +0600

    connect client site

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
 
 const users = [
   { id: 1, name: "John", email: "john@gmail.com" },

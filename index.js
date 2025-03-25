d656d2beca8a4c901abc005ae53805481b202d61
anikatasnin <jarintasnin09@gmail.com>
2025-03-25 19:56:30 +0600

    create users json data

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = [
+  { id: 1, name: "John", email: "john@gmail.com" },
+  { id: 1, name: "Abraham", email: "john@gmail.com" },
+  { id: 1, name: "A", email: "a@gmail.com" },
+  { id: 1, name: "B", email: "b@gmail.com" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
